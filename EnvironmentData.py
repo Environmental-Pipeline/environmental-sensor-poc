18675629acb8992cf6bc8f62b3eb75383cd0c470
Bryce Chamberlain <superchordate@gmail.com>
2024-10-04 12:45:26 -0600

    Initialize database upon class init.

--- EnvironmentData.py
+++ EnvironmentData.py
@@ -3,79 +3,93 @@ from tqdm import tqdm
 
 class EnvironmentData():
 
-    def __init__(self, CatsUserID):
+    def __init__(self, CatsUserID, data_path = 'data/', days_back = 90, testing = False):
 
+        if not os.path.exists(data_path):
+            print('Create data folder.')
+            os.makedirs(data_path)
+        
         self.apikeys = {'CORIS': os.environ.get('CORIS_API_KEY')}
         self.CatsUserID = CatsUserID
+        self.data_path = data_path
 
-    def get_current_status(self):
+        self.initialize_database(days_back = days_back, testing = testing)
 
-        url = f'https://cats.corismonitoring.com/api/cats/user/?ApiKey={self.apikeys['CORIS']}&CatsUserID={self.CatsUserID}'
+    def get_sensor_ids(self, testing = False):
+
+        url = f'https://cats.corismonitoring.com/api/cats/user/?ApiKey={self.apikeys["CORIS"]}&CatsUserID={self.CatsUserID}'
         response = requests.get(url)
-        self.current_status = polars.DataFrame(response.json()['Sensors'])
-        self.sensor_ids = {
-            'Temperature': self.current_status.filter(polars.col('SensorType') == 'Temperature')['SensorID'].unique(),
-            'Humidity': self.current_status.filter(polars.col('SensorType') =='Humidity')['SensorID'].unique()
+        current_status = polars.DataFrame(response.json()['Sensors'])
+        sensor_ids = {
+            'Temperature': current_status.filter(polars.col('SensorType') == 'Temperature')['SensorID'].unique(),
+            'Humidity': current_status.filter(polars.col('SensorType') =='Humidity')['SensorID'].unique()
         }
-        return self.current_status
-
-    def get_historical_status(self, days_back, testing = False):
 
         if testing:
-            for key in self.sensor_ids:        
-                    self.sensor_ids[key] = self.sensor_ids[key][0:10]
+            for key in sensor_ids:        
+                    sensor_ids[key] = sensor_ids[key][0:10]
 
+        return sensor_ids
+
+
+    def get_current_status(self):
+        
+        url = f'https://cats.corismonitoring.com/api/cats/user/?ApiKey={self.apikeys["CORIS"]}&CatsUserID={self.CatsUserID}'
+        response = requests.get(url)
+        current_status = polars.DataFrame(response.json()['Sensors'])
+        return current_status
+
+    def initialize_database(self, days_back, testing = False):
+
+        sensor_ids = []
         current_utc = int(time.time())
         start_utc = current_utc - days_back * 24 * 60 * 60
 
-        pbar = tqdm(total = len(self.sensor_ids['Temperature']) + len(self.sensor_ids['Humidity']), desc="Gather historical readings")
-        readings = {'Temperature': [], 'Humidity': []}
-        for sensor_id in self.sensor_ids['Temperature']:
-            url = '&'.join([
-                f'https://cats.corismonitoring.com/api/sensor/historical/?ApiKey={self.apikeys['CORIS']}',
-                f'SensorID={sensor_id}',
-                f'ReadingType=SensorReadingF',
-                f'StartUTC={start_utc}',
-                f'EndUTC={current_utc}',
-                f'MinReadingSpacing=600', # every 10 minutes.
-                f'RequestedOutputFormat=raw'
-            ])
-            response = requests.get(url)
-            if not response.ok:
-                raise Exception(response.json())
-            data = polars.read_csv(response.content, has_header = False)
-            data.columns = ['UTC', 'Reading']
-            data = data.with_columns(polars.lit(sensor_id).alias('SensorID'))
-            data = data.with_columns(polars.col("Reading").cast(polars.Float32))
-            readings['Temperature'].append(data.drop_nulls())
-            pbar.update(1)
-
-        for sensor_id in self.sensor_ids['Humidity']:
-            url = '&'.join([
-                f'https://cats.corismonitoring.com/api/sensor/historical/?ApiKey={self.apikeys['CORIS']}',
-                f'SensorID={sensor_id}',
-                f'ReadingType=SensorReadingRh',
-                f'StartUTC={start_utc}',
-                f'EndUTC={current_utc}',
-                f'MinReadingSpacing=600', # every 10 minutes.
-                f'RequestedOutputFormat=raw'
-            ])
-            response = requests.get(url)
-            if not response.ok:
-                raise Exception(response.json())
-            data = polars.read_csv(response.content, has_header = False)
-            data.columns = ['UTC', 'Reading']
-            data = data.with_columns(polars.lit(sensor_id).alias('SensorID'))
-            data = data.with_columns(polars.col("Reading").cast(polars.Float32))
-            readings['Humidity'].append(data.drop_nulls())    
-            pbar.update(1)
-
-        pbar.close()
-
+        # get historical readings.
+        readings = {
+            'Temperature': {'ReadingType': 'SensorReadingF', 'data': []}, 
+            'Humidity':{'ReadingType': 'SensorReadingRh', 'data': []}
+        }
         for key in readings:
-            readings[key] = polars.concat(readings[key])
 
-        self.historical_readings = readings
-        return self.historical_readings
+            # if the data already exists, skip this reading type.
+            readings[key]['datafile'] = f'{self.data_path}/{key}{"-testing" if testing else ""}.parquet'
+            if os.path.exists(readings[key]['datafile']):
+                continue
+
+            # get sensor ids here to prevent doing it if we don't actually need any new data. 
+            if len(sensor_ids) == 0:
+                print('Get sensor Ids.')
+                sensor_ids = self.get_sensor_ids(testing = testing)
+
+            pbar = tqdm(total = len(sensor_ids[key]), desc=f'Gather readings for {key}')
+            for sensor_id in sensor_ids[key]:
+                url = '&'.join([
+                    f'https://cats.corismonitoring.com/api/sensor/historical/?ApiKey={self.apikeys["CORIS"]}',
+                    f'SensorID={sensor_id}',
+                    f'ReadingType={readings[key]["ReadingType"]}',
+                    f'StartUTC={start_utc}',
+                    f'EndUTC={current_utc}',
+                    f'MinReadingSpacing=600', # every 10 minutes.
+                    f'RequestedOutputFormat=raw'
+                ])
+                response = requests.get(url)
+                if not response.ok:
+                    raise Exception(response.json())
+                data = polars.read_csv(response.content, has_header = False)
+                data.columns = ['UTC', 'Reading']
+                data = data.with_columns(polars.lit(sensor_id).alias('SensorID'))
+                data = data.with_columns(polars.col("Reading").cast(polars.Float32))
+                readings[key]['data'].append(data.drop_nulls())
+                pbar.update(1)
+
+            pbar.close()
+
+        # combine readings from multiple sensors into one dataframe.
+        # initialize database.
+        for key in readings:
+            if len(readings[key]['data']) > 0:
+                print(f'Initialize database for {key}.')
+                polars.concat(readings[key]['data']).write_parquet(readings[key]['datafile'])
 
         
--- scratch-corisapi.py
+++ scratch-corisapi.py
@@ -1,6 +1,4 @@
 from EnvironmentData import EnvironmentData
 
 api = EnvironmentData(CatsUserID = 2496)
-current = api.get_current_status()
-history = api.get_historical_status(days_back = 90, testing = True)
 
